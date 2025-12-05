15535ba2cdbc9d29ff86b6a480ff439ba8e3aa41
einar <einar.j.holsbo@uit.no>
2025-12-05 11:58:06 +0100

    add part 2 day 3

--- dec_03/main.rkt
+++ dec_03/main.rkt
@@ -6,6 +6,9 @@
 ; since order matters the last digit can't be in the 10 position but otherwise 
 ; you want to maximize this since no smaller number can make anything larger no 
 ; matter what you choose for the 1 position
+;
+; part 2 is just the same kind of thinking  but in step 1 exclude the last 
+; 11 digits, step 2 exclude the last 10, etc. etc.
 
 ; this is from day 2, splits a string into n-character strings
 (define (partition str n) 
@@ -15,18 +18,27 @@
         (part (substring str n) (cons (substring str 0 n) lst))))
   (reverse (part str '())))
 
-; maximize jolts in bank (str)
-(define (joltmaxx str)
+; maximize jolts in bank (str) over n batteries
+(define (joltmaxx str n)
   ; splits string into list of digits
-  (define lst (map string->number (partition str 1)))
+  (define bank (map string->number (partition str 1)))
+
+  ; a recursive maximizer 
+  (define (jmaxx lst n solution) 
+    (define maxest (argmax values (take lst (- (length lst) (sub1 n)))))
+    (define new-solution (+ (* solution 10) maxest))
+    (if (equal? n 1) 
+        new-solution
+        (jmaxx (cdr (member maxest lst)) (sub1 n) new-solution)))
+  (jmaxx bank n 0))
+
   ; remove last digit, find max
-  (define tens (argmax values (cdr (reverse lst))))
+  ; (define tens (argmax values (cdr (reverse lst))))
   ; remove first part of list up to and including tens; find max
-  (define ones (argmax values (cdr (member tens lst))))
-  (+ (* 10 tens) ones))
+  ; (define ones (argmax values (cdr (member tens lst))))
+  ; (+ (* 10 tens) ones))
 
 (define banks (file->lines "input.txt"))
-(foldl + 0 (map joltmaxx banks))
+(foldl + 0 (map (lambda (x) (joltmaxx x 12)) banks))
 
-; part 2 is just the same kind of thinking  but in step 1 exclude the last 
-; 11 digits, step 2 exclude the last 10, etc. etc.
+
