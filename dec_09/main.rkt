1ff7859e16ec5bfce329541329062ca1ba48679f
3inar <einar.j.holsbo@uit.no>
2025-12-13 21:27:06 +0100

    add part 2, could use some cleanup

--- dec_09/main.rkt
+++ dec_09/main.rkt
@@ -20,19 +20,13 @@
 
 
 ; for part 2 we need to check that all points inside the area are within the 
-; perimeter defined by the tile list. we can do it by the ray trace parity check
+; perimeter defined by the tile list. it requires a lot of resources to do this 
+; by the parity check
 (define (edgelist tl acc) 
   (if (< (length tl) 2) 
     acc
     (edgelist (cdr tl) (cons (take tl 2) acc))))
 
-
-(define (points-in-rect c1 c2)
-  (define xmin (min (first c1) (first c2)))
-  (define xmax ((compose add1 max) (first c1) (first c2)))
-  (define ymin (min (second c1) (second c2)))
-  (define ymax ((compose add1 max) (second c1) (second c2)))
-  (apply append (map (lambda (x) (map (lambda (y) (list x y)) (range ymin ymax))) (range xmin xmax))))
 
 ; '((  x1   y1) (  x2   y2))
 (define (horizontal? edge)
@@ -103,32 +97,85 @@
                 (if (vertical? (car edgelist)) (cons (car edgelist) verticals) verticals))]))
   (checker edgelist '()))
 
-(define (r/g/memo? pt edgelist hmap) 
-  (cond [(not (hash-has-key? hmap pt)) (hash-set! hmap pt (red/green? pt edgelist))])
-  (hash-ref hmap pt))
 
-(define (valid-square? c1 c2 edges) 
-  (andmap (lambda (x) (r/g/memo? x edges hmap)) (points-in-rect c1 c2)))
+; an edge is outside a square if both x coordinates or both y coordinates are
+; on the same side of the square
+(define (edge-in-square? e c1 c2)
+  (define (both<= x1 x2 edg)
+    (and (<= x1 edg) (<= x2 edg)))
+  (define (both>= x1 x2 edg)
+    (and (>= x1 edg) (>= x2 edg)))
+  (define xmin (min (first c1) (first c2)))
+  (define xmax (max (first c1) (first c2)))
+  (define ymin (min (second c1) (second c2)))
+  (define ymax (max (second c1) (second c2)))
+  (not (or (both<= (first (first e)) (first (second e)) xmin)
+           (both>= (first (first e)) (first (second e)) xmax)
+           (both<= (second (first e)) (second (second e)) ymin)
+           (both>= (second (first e)) (second (second e)) ymax))))
 
-; it's just p1 with a check for valid square, in which case it reports a negative area
-(define (areas2 tile-list edges) 
-  (define (car-v-cdr ls)
-    (define t1 (car ls))
-    (map 
-      (lambda (t2) 
-        (let ([ar (area t1 t2)]) (cons (list t1 t2) (if (valid-square? t1 t2 edges) ar (- 0 ar)))))
-      (cdr ls)))
-  (define (check-pairs tls out) 
-    (if 
-      (< (length tls) 2) out
-      (check-pairs (cdr tls) (append (car-v-cdr tls) out))))
-  (check-pairs tile-list '()))
 
-(define hmap (make-hash))
+(define (square-valid? c1 c2 edges) 
+  (define (get-inner-point c1 c2) 
+    (define xmin (min (first c1) (first c2)))
+    (define ymin (min (second c1) (second c2)))
+    (list (add1 xmin) (add1 ymin)))
+  (define contains-edge? (ormap (lambda (e) (edge-in-square? e c1 c2)) edges))
+  (define (inner-point-valid? c1 c2) (red/green? (get-inner-point c1 c2) edges))
+  (and (not contains-edge?) (inner-point-valid? c1 c2)))
+
+
 (define tiles (map c->num (file->lines "input.txt")))
 (define edges (edgelist (cons (car tiles) (reverse tiles)) '()))
 (define arealist (sort (areas tiles) (lambda (a b) (> (cdr a) (cdr b)))))
 
-(car arealist) ; p1
+(car arealist) 
+(ormap (lambda (x) (if (square-valid? (first (car x)) (second (car x)) edges) x #f)) arealist)
+
+
+
+; observation: there is no proposed square entirely outside of the polygon of
+; interest since the edges of the polygon contain all possible corners
+; if a suqare has a polygon edge in its interior it can't be valid since once 
+; side of the edge is inside the poly and the other is outside. on the other 
+; hand if there is no polygon edge in the interior the whole interior is either 
+; entirely inside or entirely outside the polygon
+;
+;   invalid no matter what is
+;   inside/outside of the 
+;   polygon described by ###
+;   ......................
+;   ...#######............
+;   ......ooo#oo..........
+;   ......o..##o##........
+;   ......o....o..........
+;
+;   valid no matter what is
+;   inside/outside of the 
+;   polygon described by ###
+;   because the square can't be 
+;   on the outside
+;   ......................
+;   ...#######............
+;   ...ooo...#............
+;   ...o.o...#####........
+;   ...ooo................
+; 
+;   all interior
+;   points must either inside
+;   or outside the poly so it's
+;   enough to check one point
+;   .........oooo.........
+;   ...######o..o.........
+;   .........#..o.........
+;   .........o#o#.........
+;   ......................
+; 
+;   I don't think it is possible 
+;   have all interior points in 
+;   the square inside the polygon
+;   but some points on the edge not 
+;   in there. if possible it's 
+;   probably quite rare 
 
 
